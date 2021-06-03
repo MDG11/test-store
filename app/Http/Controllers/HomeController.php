757725ea3c27e979269a268e85a54883c5df8b92
sasha <sachaostap@gmail.com>
2021-06-03 20:15:43 +0300

    added main page products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('home.index');
+        $products = Product::orderBy('updated_at')->take(8)->get();
+           
+        return view('home.index', compact('products'));
     }
 }
