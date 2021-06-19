546ef1f645b6de55f69139eef594131c4e8673ac
sasha <sachaostap@gmail.com>
2021-06-19 22:43:25 +0300

    Comments-emails made

--- app/Http/Livewire/Admin/AdminDashboardComponent.php
+++ app/Http/Livewire/Admin/AdminDashboardComponent.php
@@ -8,6 +8,7 @@ class AdminDashboardComponent extends Component
 {
     public function render()
     {
+        $token = 
         return view('livewire.admin.admin-dashboard-component')->extends('layouts.main')->section('content');
     }
 }
